15cf314911974277ea025e717af6905418956433
yaoxin1 <yaoxin.jing1@huawei.com>
2023-06-05 13:28:42 +0200

    add artifacts

--- install_secrets.sh
+++ install_secrets.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 set -e 
-# example of using arguments to a script
-mkdir -p resource
-mkdir -p /opt/confidential-containers/kbs/repository/quark_mongo
+mkdir -p nginx_resource
+mkdir -p /opt/confidential-containers/kbs/repository/quark_nginx
 
 pushd secret
-
 for filename in *; do
-
-# echo "$filename"
-# read -rd '' content < $filename
-
-# jq -r '@base64' $filename > ../resource/$filename
-cat $filename | base64 | tr -d '\n' > ../resource/$filename
-
-# cp --parent ../quark_secret/$filename /opt/confidential-containers/kbs/repository/quark_mongo
+cat $filename | base64 | tr -d '\n' > ../nginx_resource/$filename
 done
-
-
 popd
 
-cp -R resource  /opt/confidential-containers/kbs/repository/quark_mongo
+cp -R nginx_resource  /opt/confidential-containers/kbs/repository/quark_nginx
+
